6dd8156708aa9349dee860946678ed3ed75e70b0
afilipovski <filipovskia@yahoo.com>
2023-12-13 16:34:32 +0100

    description change frontend

--- domasna3/culturedge-front/src/app/popup-content/popup-content.component.ts
+++ domasna3/culturedge-front/src/app/popup-content/popup-content.component.ts
@@ -6,16 +6,12 @@ import { AfterContentInit, Component, OnInit } from '@angular/core';
   templateUrl: './popup-content.component.html',
   styleUrls: ['./popup-content.component.css']
 })
-export class PopupContentComponent implements OnInit, AfterContentInit {
+export class PopupContentComponent implements AfterContentInit {
   constructor(
     private http: HttpClient
   ) {}
     
-  baseUrl = 'http://localhost:8080/photo'
-
-  ngOnInit(): void {
-    
-  }
+  baseUrl = 'http://localhost:8080'
 
   placeName = ""
   pictureUrl = ""
@@ -25,6 +21,11 @@ export class PopupContentComponent implements OnInit, AfterContentInit {
     if (!this.placeName)
       return;
     this.refreshPhoto();
+    this.http.get(`${this.baseUrl}/description?name=${this.placeName}`,{responseType: 'text'}).subscribe(k => {
+      this.description = k;
+      console.log(this.description);
+            
+    });
   }
 
   selectedFile! : File;
@@ -33,18 +34,24 @@ export class PopupContentComponent implements OnInit, AfterContentInit {
   }
 
   onSubmit() {
-    console.log(this.selectedFile);
-    
-    const photoUploadData = new FormData();
-    photoUploadData.append('file',this.selectedFile,this.selectedFile.name);
-    photoUploadData.append('name',this.placeName);
-
-    this.http.post(this.baseUrl,photoUploadData,{responseType: 'text'}).subscribe(k => {
-      this.refreshPhoto();
-    })
+    if (this.selectedFile) {
+      const photoUploadData = new FormData();
+      photoUploadData.append('file',this.selectedFile,this.selectedFile.name);
+      photoUploadData.append('name',this.placeName);
+      this.http.post(`${this.baseUrl}/photo`,photoUploadData,{responseType: 'text'}).subscribe(k => {
+        this.refreshPhoto();
+      })
+    }
+    if (this.description) {
+      const descriptionData = new FormData();
+      descriptionData.append('name',this.placeName);
+      descriptionData.append('desc',this.description)
+      this.http.post(`${this.baseUrl}/description`,descriptionData,{responseType: 'text'}).subscribe(k => {
+      });
+    }
   }
 
   refreshPhoto() {
-    this.pictureUrl = `${this.baseUrl}?name=${this.placeName}&lastmod=${Date.now()}`
+    this.pictureUrl = `${this.baseUrl}/photo?name=${this.placeName}&lastmod=${Date.now()}`
   }
 }
