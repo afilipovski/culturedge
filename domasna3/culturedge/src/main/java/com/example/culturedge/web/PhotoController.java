694169741a27cb86ce2c0509591e29d3e92fd37e
afilipovski <filipovskia@yahoo.com>
2023-12-13 09:13:16 +0100

    backend for image upload

--- domasna3/culturedge/src/main/java/com/example/culturedge/web/PhotoController.java
+++ domasna3/culturedge/src/main/java/com/example/culturedge/web/PhotoController.java
@@ -1,31 +1,38 @@
 package com.example.culturedge.web;
 
-import com.example.culturedge.service.PhotoService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class PhotoController {
-    final PhotoService photoService;
-
-    public PhotoController(PhotoService photoService) {
-        this.photoService = photoService;
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/images";
+    @GetMapping(value = "/photo", produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getPhoto(@RequestParam String name) throws IOException {
+        UUID uuid = UUID.nameUUIDFromBytes(name.getBytes());
+        File f = new File(UPLOAD_DIRECTORY + "\\" + uuid + ".jpg");
+        if (!f.exists())
+            throw new ResponseStatusException(NOT_FOUND);
+        try (FileInputStream fis = new FileInputStream(f)){
+            return fis.readAllBytes();
+        }
     }
 
-    @GetMapping("/photo")
-    public List<String> getPhotos(@RequestParam String name) {
-        return photoService.getByName(name);
-    }
-
-    @PostMapping("/photo/add")
-    public List<String> addPhoto(@RequestParam String name, @RequestParam String url) {
-        return photoService.addByName(name,url);
-    }
-
-    @PostMapping("/photo/delete")
-    public List<String> deletePhoto(@RequestParam String name, @RequestParam String url) {
-        return photoService.deleteByName(name,url);
+    @PostMapping(value = "/photo", produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] setPhoto(@RequestParam String name, @RequestParam MultipartFile file) throws IOException {
+        UUID uuid = UUID.nameUUIDFromBytes(name.getBytes());
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, uuid+".jpg");
+        Files.write(fileNameAndPath, file.getBytes());
+        return getPhoto(name);
     }
 }
