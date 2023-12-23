76d3243e501bb20f7042fc821077a9852052b754
stanchevskid <101475851+stanchevskid@users.noreply.github.com>
2023-12-23 20:18:13 +0100

    Data annotation and database table creation.

--- domasna3/culturedge/src/main/java/com/example/culturedge/models/CulturalHeritage.java
+++ domasna3/culturedge/src/main/java/com/example/culturedge/models/CulturalHeritage.java
@@ -1,11 +1,25 @@
 package com.example.culturedge.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "cultural_heritage")
 public class CulturalHeritage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long Id;
+
     public String name;
     public double lat;
     public double lon;
     public String historic;
     public String tourism;
+
+    public CulturalHeritage() {
+    }
 
     public CulturalHeritage(String name, double lat, double lon, String historic, String tourism) {
         this.name = name;
