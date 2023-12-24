239b7ae27a6e4f5b6a536bf23c52de1d6656baf4
stanchevskid <101475851+stanchevskid@users.noreply.github.com>
2023-12-24 18:12:27 +0100

    Modified backend to work with PostgreSQL database.

--- domasna3/culturedge/src/main/java/com/example/culturedge/service/impl/DescriptionServiceImpl.java
+++ domasna3/culturedge/src/main/java/com/example/culturedge/service/impl/DescriptionServiceImpl.java
@@ -1,24 +1,35 @@
 package com.example.culturedge.service.impl;
 
+import com.example.culturedge.models.CulturalHeritage;
+import com.example.culturedge.models.Description;
 import com.example.culturedge.repository.DescriptionRepository;
+import com.example.culturedge.repository.HeritageRepository;
 import com.example.culturedge.service.DescriptionService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DescriptionServiceImpl implements DescriptionService {
     private final DescriptionRepository descriptionRepository;
+    private final HeritageRepository heritageRepository;
 
-    public DescriptionServiceImpl(DescriptionRepository descriptionRepository) {
+    public DescriptionServiceImpl(DescriptionRepository descriptionRepository, HeritageRepository heritageRepository) {
         this.descriptionRepository = descriptionRepository;
+        this.heritageRepository = heritageRepository;
     }
 
     @Override
     public String getByName(String name) {
-        return descriptionRepository.getByName(name);
+        CulturalHeritage culturalHeritage = this.heritageRepository.findByName(name);
+        if (descriptionRepository.findByCulturalHeritage(culturalHeritage) != null) {
+            return descriptionRepository.findByCulturalHeritage(culturalHeritage).description;
+        } else {
+            return "";
+        }
     }
 
     @Override
     public void setByName(String name, String desc) {
-        descriptionRepository.setByName(name,desc);
+        CulturalHeritage culturalHeritage = this.heritageRepository.findByName(name);
+        descriptionRepository.save(new Description(culturalHeritage, desc));
     }
 }
