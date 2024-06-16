9ca999ae3e0b06441aaccebbc212493616836b6d
afilipovski <filipovskia@yahoo.com>
2024-06-16 14:24:01 +0200

    Removed email details

--- microservice-architecture/feedback-microservice/src/main/java/com/example/feedbackmicroservice/service/impl/EmailServiceImpl.java
+++ microservice-architecture/feedback-microservice/src/main/java/com/example/feedbackmicroservice/service/impl/EmailServiceImpl.java
@@ -7,21 +7,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
-
-    private final JavaMailSender javaMailSender;
-
     public EmailServiceImpl(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
     }
 
     // Sends an email with the provided email address and message content.
     public void sendEmail(String email, String message) {
 
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("culturedge24@gmail.com");
-        mailMessage.setTo("culturedge24@gmail.com");
-        mailMessage.setSubject(String.format("Feedback from: %s",email));
-        mailMessage.setText(message);
-        javaMailSender.send(mailMessage);
     }
 }
