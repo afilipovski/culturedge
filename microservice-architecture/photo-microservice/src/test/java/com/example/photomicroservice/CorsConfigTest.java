c0c8579eecf98f1538371cfaa4f9260a9963c3f5
afilipovski <filipovskia02@gmail.com>
2024-07-06 20:05:06 +0200

    tests added for photo-microservice

---
+++ microservice-architecture/photo-microservice/src/test/java/com/example/photomicroservice/CorsConfigTest.java
@@ -0,0 +1,29 @@
+package com.example.photomicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CorsConfigTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testCorsConfiguration() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.options("/api/photo")
+                        .header("Origin", "https://example.com")
+                        .header("Access-Control-Request-Method", "POST"))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Access-Control-Allow-Origin", "*"))
+                .andExpect(header().string("Access-Control-Allow-Methods", "OPTIONS,GET,POST,PUT,DELETE"));
+    }
+}
